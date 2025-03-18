cbd61606b8ff9a6afeb9ad6813af8cd4672c22c5
Gabriel Mislang <gabrielmislang101@gmail.com>
2025-03-18 18:05:02 +0800

    feat: add main svelte site

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import 'leaflet/dist/leaflet.css';
+    import Map from '../components/Map.svelte'
+	import { onMount } from 'svelte';
+
+
+    // let jeepMarkers = [
+    //     { lat: 14.648792269553422, lng: 121.06847648049134, popup: 'Jeepney #1' }, // DCS
+    //     { lat: 14.655, lng: 121.07, popup: 'Jeepney #2' }
+    // ];
+
+    let jeepMarkers = [];
+
+    onMount(() => {
+        // Fetch markers initiallly
+        fetchMarkers();
+        
+        // Poll every 5 secs
+        const interval = setInterval(fetchMarkers, 5000);
+
+        return () => clearInterval(interval);
+    });
+
+    async function fetchMarkers() {
+        const res = await fetch('http://127.0.0.1:8000/api/jeepneys'); // Server endpoint
+        const data = await res.json();
+        console.log('Fetched markers:', data);
+        jeepMarkers = data;
+    }
+</script>
+
+<h1>Map</h1>
+
+<Map center={[14.655, 121.066]} zoom={15.5} markers={jeepMarkers} />
