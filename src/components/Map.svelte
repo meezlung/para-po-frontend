a6ff39852d5bfe4789b7467ebec743009328ecf7
meezlung <gabrielmislang101@gmail.com>
2025-05-30 23:27:52 +0800

    feat: add "you" icon, add jeep label

--- src/components/Map.svelte
+++ src/components/Map.svelte
@@ -5,18 +5,28 @@
 
     const jeepIcon = L.icon({
         iconUrl: '/jeepney.png',
-        iconSize: [40, 40],
-        iconAnchor: [20, 40],
-        popupAnchor: [0, -40],   
+        iconSize: [25, 25],
+        iconAnchor: [10, 10],
+        popupAnchor: [0, 0],   
     })
+    
+    // a new “you are here” icon
+    const meIcon = L.icon({
+        iconUrl: '/me.png',
+        iconSize: [16, 16],
+        iconAnchor: [8, 8],
+        popupAnchor: [0, 0],
+    });
 
     export let center = [14.652, 121.065]; // Near UP Diliman
     export let zoom = 14;
     export let markers = []; // [{lat: 0, lng: 0, popup: 'Jeep 1'}, ...]
+    export let myPosition = null; // { lat, lng } or null
 
     let map;
     let mapElement; // Reference to div container for the map
     let markerLayer; // We'll store a LayerGroup for easy updates
+    let meMarker;
 
     onMount(() => {
         // Initialize the leaflet map
@@ -31,6 +41,19 @@
             attribution:
                 '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(map);
+
+        // L.tileLayer.provider('CartoDB.Positron').addTo(map);
+
+        // L.tileLayer.provider('Esri.WorldImagery').addTo(map);
+
+        // L.tileLayer.provider('OpenTopoMap').addTo(map);
+
+        // L.tileLayer.provider('Stadia.StamenWatercolor').addTo(map);
+
+        // L.tileLayer('https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png', {
+        //     maxZoom: 20,
+        //     attribution: '<a href="https://github.com/cyclosm/cyclosm-cartocss-style/releases" title="CyclOSM - Open Bicycle render">CyclOSM</a> | Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        // }).addTo(map);
 
         // Automatically plot markers
         markerLayer = L.layerGroup().addTo(map);
@@ -52,12 +75,45 @@
         // Add new markers from the updated array
         markers.forEach(({ lat, lng, popup }) => {
             const marker = L.marker([lat, lng], { icon: jeepIcon });
+            
+            // label ng jeep
             if (popup) {
                 marker.bindPopup(popup);
             }
+            
+            // add label
+            marker.bindTooltip(popup, {
+                permanent: true,
+                direction: 'top',
+                offset: [0, -10],
+                className: 'jeep-label'
+            });
+
+            // add the marker to the map
             marker.addTo(markerLayer);
         })
     };
+
+    $: if (map && myPosition) {
+        const { lat, lng } = myPosition;
+        
+        if (meMarker) {
+            // just move it
+            meMarker.setLatLng([lat,lng]);
+        } else {
+            // create it once
+            meMarker = L.marker([lat, lng], { icon: meIcon })
+
+            // add popout sticking out
+            meMarker.bindTooltip("You are here!", {
+                permanent: true,
+                direction: 'top',
+                offset: [0, -10],
+            });
+            
+            meMarker.addTo(map);
+        }
+    }
 </script>
 
 <style>
@@ -67,6 +123,5 @@
         height: 90vh;
     }
 </style>
-  
 
 <div bind:this={mapElement} class="map-container"></div>
